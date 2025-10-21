2ce03fb3efe2d581a2cb960536b52d10d7b60923
rifa-sanjida <23101062@uap-bd.edu>
2025-10-21 22:08:35 +0600

    Update views.py

--- swapdonaterent_project/core/views.py
+++ swapdonaterent_project/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def privacy_policy(request):
+    return render(request, 'core/privacy_policy.html')
+
+def terms_conditions(request):
+    return render(request, 'core/terms_conditions.html')
