df9222fdfa714d2219249e04f4ee7cd17f8b0bc3
rifa-sanjida <23101062@uap-bd.edu>
2025-10-21 21:57:40 +0600

    Update admin.py

--- swapdonaterent_project/core/admin.py
+++ swapdonaterent_project/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Cart, CartItem
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at']
+    list_filter = ['created_at']
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'item', 'added_at']
+    list_filter = ['added_at']
