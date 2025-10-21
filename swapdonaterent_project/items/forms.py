3e7b4fc79cf4b4732a32b93009aa4995adeb99dc
rifa-sanjida <23101062@uap-bd.edu>
2025-10-21 22:10:58 +0600

    Create forms.py

---
+++ swapdonaterent_project/items/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Item, Message
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['category', 'item_type', 'name', 'description', 'price', 'condition', 'location', 'contact_info', 'image']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+            'contact_info': forms.Textarea(attrs={'rows': 3}),
+        }
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Type your message here...'}),
+        }
