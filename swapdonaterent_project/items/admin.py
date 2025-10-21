0c48794ce996393163e34ca941469a1ee370c3f4
rifa-sanjida <23101062@uap-bd.edu>
2025-10-21 22:10:03 +0600

    Update admin.py

--- swapdonaterent_project/items/admin.py
+++ swapdonaterent_project/items/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Item, Conversation, Message
 
-# Register your models here.
+
+admin.site.register(Item)
+admin.site.register(Conversation)
+admin.site.register(Message)
