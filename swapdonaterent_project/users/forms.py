4c08d38c2b5bbc976bd5a9e7d7ecb1df76fe6c63
creep1303 <mithilarahman1303@gmail.com>
2025-10-23 19:10:18 +0600

    Update forms.py

--- swapdonaterent_project/users/forms.py
+++ swapdonaterent_project/users/forms.py
@@ -12,3 +12,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'phone', 'address', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone', 'address', 'bio', 'profile_picture']
