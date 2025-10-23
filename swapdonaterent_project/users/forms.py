2d342a72a127abb54123ffeae2409d0898fae20f
mariamalam <23101080@uap-bd.edu>
2025-10-23 18:55:57 +0600

    Update forms.py

--- swapdonaterent_project/users/forms.py
+++ swapdonaterent_project/users/forms.py
@@ -12,22 +12,3 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'phone', 'address', 'password1', 'password2']
-
-
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email']
-
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['phone', 'address', 'bio', 'profile_picture']
-
-
-class AccountDeleteForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
